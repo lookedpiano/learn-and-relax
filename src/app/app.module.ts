ce2dbdf7c133db46937e2458ee1c33adde0158a8
lookedpiano <jimmy.friedrich@hotmail.ch>
2022-04-21 15:22:54 +0200

    header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EquationComponent } from './equation/equation.component';
 import { AnswerHighlightDirective } from './answer-highlight.directive';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EquationComponent,
-    AnswerHighlightDirective
+    AnswerHighlightDirective,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
