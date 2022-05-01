715a36c09cd9f34bd1d3b5e5de3d3c5fbe42e8bf
lookedpiano <jimmy.friedrich@hotmail.ch>
2022-05-01 14:12:18 +0200

    show/hide logic of components

--- src/app/decision/decision.component.ts
+++ src/app/decision/decision.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DecisionComponent implements OnInit {
 
+  mathRoute: string = '/equation';
+
+  mathCompShown: boolean = true;
+
+  mathCompShownFun() {
+    this.mathCompShown = !this.mathCompShown;
+    this.mathRoute = this.mathCompShown? 'equation': '';
+    return this.mathRoute;
+  }
+
+  photoRoute: string = '/photos';
+
+  photoCompShown: boolean = true;
+
+  photoCompShownFun() {
+    this.photoCompShown = !this.photoCompShown;
+    this.photoRoute = this.photoCompShown? 'photos': '';
+    return this.photoRoute;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
